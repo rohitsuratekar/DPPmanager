569542feff2dae3ed6981c2674afd8aa842d6362
Rohit Suratekar <rohitsuratekar@gmail.com>
2016-05-10 09:38:12 +0530

    Added plugin interface file

---
+++ src/com/secretbiology/DPPmanager/Main.java
@@ -0,0 +1,51 @@
+package com.secretbiology.DPPmanager;
+import org.micromanager.api.MMPlugin;
+
+
+public class Main implements MMPlugin{
+
+    public static void main(String[] args) {
+
+	// write your code here
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void setApp(ScriptInterface app) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void configurationChanged() {
+
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public String getVersion() {
+        return null;
+    }
+
+    @Override
+    public String getCopyright() {
+        return null;
+    }
+}
