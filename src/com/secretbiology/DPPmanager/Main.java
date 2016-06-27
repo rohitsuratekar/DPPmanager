8faa1ed0093dfc58a917d88c3dfe6b45bf6d42cc
Rohit Suratekar <rohitsuratekar@gmail.com>
2016-06-27 19:15:21 +0530

    Added external library.

--- src/com/secretbiology/DPPmanager/Main.java
+++ src/com/secretbiology/DPPmanager/Main.java
@@ -1,51 +1,66 @@
 package com.secretbiology.DPPmanager;
+import java.io.PrintStream;
+
+import mmcorej.CMMCore;
+
 import org.micromanager.api.MMPlugin;
+import org.micromanager.api.ScriptInterface;
+import javax.swing.JOptionPane;
 
+public class Main implements MMPlugin {
+    public static final String menuName = "Hello World";
+    public static final String tooltipDescription =
+            "Displays a simple dialog";
 
-public class Main implements MMPlugin{
+    // Provides access to the Micro-Manager Java API (for GUI control and high-
+    // level functions).
+    private ScriptInterface app_;
+    // Provides access to the Micro-Manager Core API (for direct hardware
+    // control)
+    private CMMCore core_;
 
     public static void main(String[] args) {
 
-	// write your code here
+        // write your code here
+    }
+
+
+    @Override
+    public void setApp(ScriptInterface app) {
+        app_ = app;
+        core_ = app.getMMCore();
     }
 
     @Override
     public void dispose() {
-
-    }
-
-    @Override
-    public void setApp(ScriptInterface app) {
-
+        // We do nothing here as the only object we create, our dialog, should
+        // be dismissed by the user.
     }
 
     @Override
     public void show() {
-
+        JOptionPane.showMessageDialog(null, "Hello, world!", "Hello world!",
+                JOptionPane.PLAIN_MESSAGE);
     }
 
     @Override
-    public void configurationChanged() {
-
+    public String getInfo () {
+        return "Displays a simple greeting.";
     }
 
     @Override
     public String getDescription() {
-        return null;
-    }
-
-    @Override
-    public String getInfo() {
-        return null;
+        return tooltipDescription;
     }
 
     @Override
     public String getVersion() {
-        return null;
+        return "1.0";
     }
 
     @Override
     public String getCopyright() {
-        return null;
+        return "University of California, 2015";
     }
 }
+
